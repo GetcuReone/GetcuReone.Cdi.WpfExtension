e33c7d52f58e27b1672f047ec57456609bde05cd
Juel <GetcuReone@hotmail.com>
2020-11-20 18:39:25 +0300

    added CdiWpfHelper

---
+++ GetcuReone.Cdi.WpfExtension/GetcuReone.Cdi.WpfExtension/CdiWpfHelper.cs
@@ -0,0 +1,36 @@
+﻿using GetcuReone.Cdi.WpfEntities;
+using GetcuReone.FactFactory.BaseEntities;
+using System;
+using System.Windows.Controls;
+
+namespace GetcuReone.Cdi
+{
+    /// <summary>
+    /// Helper for GetcuReone.Cdi.Wpf.
+    /// </summary>
+    public static class CdiWpfHelper
+    {
+        /// <summary>
+        /// Create visibile scope.
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <returns></returns>
+        public static IDisposable CreateVisibleScope(this Frame frame)
+        {
+            return new VisibleFrameScope(frame);
+        }
+
+        /// <summary>
+        /// Copy container.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        public static void CopyTo<TFactContainerFrom, TFactContainerTo>(this TFactContainerFrom from, TFactContainerTo to)
+            where TFactContainerFrom : FactContainerBase
+            where TFactContainerTo : FactContainerBase
+        {
+            foreach (var fact in from)
+                to.Add(fact);
+        }
+    }
+}
