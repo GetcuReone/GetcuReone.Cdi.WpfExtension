6fc55fca6ab98f891983a13d797bd63a0c06a56e
Juel <GetcuReone@hotmail.com>
2020-11-20 18:22:29 +0300

    added Awaiter

---
+++ GetcuReone.Cdi.WpfExtension/GetcuReone.Cdi.WpfExtension/Entities/Awaiter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GetcuReone.Cdi.Entities
+{
+    /// <summary>
+    /// Object to wait.
+    /// </summary>
+    public class Awaiter
+    {
+        private readonly int _waitStep;
+        private readonly List<Awaiter> _child = new List<Awaiter>();
+
+        /// <summary>
+        /// Finished.
+        /// </summary>
+        public bool Finished { get; private set; } = false;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="waitStep"></param>
+        public Awaiter(int waitStep = 100)
+        {
+            _waitStep = waitStep;
+        }
+
+        /// <summary>
+        /// Finish.
+        /// </summary>
+        public void Finish() => Finished = true;
+
+        /// <summary>
+        /// Add child <see cref="Awaiter"/>.
+        /// </summary>
+        /// <param name="awaiter"></param>
+        public void AddChild(Awaiter awaiter) => _child.Add(awaiter);
+
+        /// <summary>
+        /// wait for the end.
+        /// </summary>
+        /// <returns></returns>
+        public async ValueTask WaitFinishAsync()
+        {
+            while (!Finished)
+                await Task.Delay(_waitStep).ConfigureAwait(false);
+
+            foreach (var item in _child)
+                while (!item.Finished)
+                    await Task.Delay(_waitStep).ConfigureAwait(false);
+        }
+    }
+}
