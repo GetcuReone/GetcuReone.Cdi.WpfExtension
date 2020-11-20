07d506599193adcbbf8f7318bdd6b1a872bd9402
Juel <GetcuReone@hotmail.com>
2020-11-20 18:27:26 +0300

    added VisibleFrameScope

--- GetcuReone.Cdi.WpfExtension/GetcuReone.Cdi.WpfExtension/WpfEntities/Awaiter.cs
+++ GetcuReone.Cdi.WpfExtension/GetcuReone.Cdi.WpfExtension/WpfEntities/Awaiter.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace GetcuReone.Cdi.Entities
+namespace GetcuReone.Cdi.WpfEntities
 {
     /// <summary>
     /// Object to wait.
