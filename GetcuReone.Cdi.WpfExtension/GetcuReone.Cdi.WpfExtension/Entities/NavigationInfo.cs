5fb21833836ec8d75abbdc2c4dc0db40082204fd
Juel <GetcuReone@hotmail.com>
2021-02-05 03:01:13 +0300

    Add navigation information Fixes

---
+++ GetcuReone.Cdi.WpfExtension/GetcuReone.Cdi.WpfExtension/Entities/NavigationInfo.cs
@@ -0,0 +1,16 @@
+﻿using GetcuReone.MvvmFrame.Wpf;
+
+namespace GetcuReone.Cdi.MvvmFrameWpf.Entities
+{
+    /// <summary>
+    /// Navigation info.
+    /// </summary>
+    public class NavigationInfo : ModelBase
+    {
+        /// <summary>
+        /// Latest navigation view-model.
+        /// </summary>
+        public GrViewModelBase LatestViewModel { get => _latestViewModel; set => SetPropertyValue(ref _latestViewModel, value); }
+        private GrViewModelBase _latestViewModel;
+    }
+}
